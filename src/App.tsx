6e02d4793bc2f9f82158421dd4e14e3bd62980ab
Stanislav <verbatim8872@gmail.com>
2021-10-07 14:46:23 +0300

    add toastify

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './App.scss'
 import AddCity from './components/AddCity'
 import WidgetsList from './components/WidgetsList'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 
 const App = () => {
     return (
         <div className='app'>
             <AddCity/>
             <WidgetsList/>
+            <ToastContainer/>
         </div>
     )
 }
--- src/components/AddCity.tsx
+++ src/components/AddCity.tsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react'
 import s from './AddCity.module.scss'
+import { toast } from 'react-toastify'
 
 const AddCity = () => {
 
     const [ inputCity, setInputCity ] = useState('')
 
+    const toastOptions = {
+        hideProgressBar: true,
+        autoClose: 2000,
+        position: 'top-center'
+    }
+
     const addCityHandler = () => {
         if(inputCity){
             console.log(inputCity)
+        }else{
+            toast('The field cannot be empty', toastOptions as {})
         }
     }
 
