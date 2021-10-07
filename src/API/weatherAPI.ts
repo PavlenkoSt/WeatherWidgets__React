e17892facb5a69070e211a59310faa76004f36f5
Stanislav <verbatim8872@gmail.com>
2021-10-07 21:44:35 +0300

    add widget data

--- src/API/weatherAPI.ts
+++ src/API/weatherAPI.ts
@@ -2,7 +2,7 @@ import config from '../config.json'
 
 const weatherAPI = {
     getWeatherByCity: async (city: string) => {
-        const responce = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${config.API_KEY}`)
+        const responce = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${config.API_KEY}&units=metric`)
         const weather = await responce.json()
         return weather
     }
--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -29,9 +29,23 @@ const Widget: FC<WidgetPropsType> = ({ widgetInfo }) => {
                     </div>
                 </div>
                 <div className={ s.date }>{ currentWeather.dt_txt }</div>
-                <Chart
-                    
-                />
+                <Chart />
+                <div className={ s.bottom }>
+                    <div>
+                        <div className={ s.temp }>
+                            <div className={ s.tempVal }>{ Math.round(currentWeather.main.temp) }</div>
+                            <div className={ s.tempTumbler }>
+                                <div className={ s.tempItem }>&deg;C</div> | <div className={ s.tempItem }>&deg;F</div>
+                            </div>
+                        </div>
+                        <div className={ s.feelsLike }>Feels like: { Math.round(currentWeather.main.feels_like) } &deg;C</div>
+                    </div>
+                    <div>
+                        <div className={ s.option }>Wind: <span>{ currentWeather.wind.speed } m/s</span></div>
+                        <div className={ s.option }>Humidity: <span>{ currentWeather.main.humidity }%</span></div>
+                        <div className={ s.option }>Pressure: <span>{ currentWeather.main.pressure }Pa</span></div>
+                    </div>
+                </div>
            </div>
         </div>
     )
