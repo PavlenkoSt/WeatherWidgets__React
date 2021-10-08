af772257033902a18b27677eaa13d2b5f9c9b4d2
Stanislav <verbatim8872@gmail.com>
2021-10-09 00:54:33 +0300

    template for authocomplete

--- src/components/AddCity.tsx
+++ src/components/AddCity.tsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react'
+import React, { FC, useState } from 'react'
 import s from './AddCity.module.scss'
 import { toast } from 'react-toastify'
 import weatherAPI from '../API/weatherAPI'
 import useAction from '../hooks/useAction'
 import { CityWeather } from '../models/weather'
 import localStorageService from '../localStorage'
+import config from '../config.json'
+import PlacesAuthocomplete from 'react-places-autocomplete'
+//@ts-ignore
+import scriptLoader from 'react-async-script-loader'
 
-const AddCity = () => {
+
+const AddCity: FC<any> = ({ isScriptLoaded, isScriptLoadSucceed }) => {
 
     const [ inputCity, setInputCity ] = useState('')
     const [ isLoading, setIsLoading ] = useState(false)
@@ -49,13 +54,52 @@ const AddCity = () => {
         }
     }
 
+    if(!isScriptLoaded || !isScriptLoadSucceed){
+        return <div className={ s.load } >Loading...</div>
+    }
+
     return (
         <div className={ s.container } >
-            <input 
+            {/* <input 
                 className={ s.input } 
                 value={ inputCity }
                 onChange={ (e) => setInputCity(e.target.value) }
-            />
+            /> */}
+            <PlacesAuthocomplete value={ inputCity } onChange={ (value) => setInputCity(value) } onSelect={ (value) => setInputCity(value) } >
+                {
+                    ({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                        <div>
+                            <input
+                                {...getInputProps({
+                                    className: s.input,
+                                  })}
+                            />
+                            <div className="autocomplete-dropdown-container">
+                                {loading && <div>Loading...</div>}
+                                {suggestions.map(suggestion => {
+                                    const className = suggestion.active
+                                        ? 'suggestion-item--active'
+                                        : 'suggestion-item';
+                                    // inline style for demonstration purpose
+                                    const style = suggestion.active
+                                        ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                        : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                                    return (
+                                        <div
+                                            {...getSuggestionItemProps(suggestion, {
+                                                className,
+                                                style,
+                                            })}
+                                        >
+                                            <span>{suggestion.description}</span>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    )
+                }
+            </PlacesAuthocomplete>
             <button 
                 className={ s.btn }
                 onClick={ addCityHandler }
@@ -65,4 +109,6 @@ const AddCity = () => {
     )
 }
 
-export default AddCity
+export default scriptLoader(
+    ['https://maps.googleapis.com/maps/api/js?key=AIzaSyAbmMW9WnFV2Or3b9w4q6UtuOyB1tQXVNY&libraries=places']
+)(AddCity)
