a61ce10b7d803b3f3d92d35d784431bc49c29622
Stanislav <verbatim8872@gmail.com>
2021-10-08 12:31:20 +0300

    format date

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -4,6 +4,7 @@ import Chart from '../Chart'
 import BottomWidget from './BottomWidget'
 import TopWidget from './TopWidget'
 import s from './Widget.module.scss'
+import dateFormat from 'dateformat'
 
 type WidgetPropsType = {
     widgetInfo: CityWeather
@@ -14,6 +15,8 @@ const Widget: FC<WidgetPropsType> = ({ widgetInfo }) => {
     const currentWeather = widgetInfo.weather[0]
     const isColdTheme = Math.round(currentWeather.main.temp) < 0
 
+    const formatedDate = dateFormat(currentWeather.dt_txt, "ddd, dd mmmm, HH:MM")
+
     return (
         <div className={ s.container }>
            <div className={ isColdTheme ? `${s.widget} ${s.cold}` : s.widget }>
@@ -21,12 +24,11 @@ const Widget: FC<WidgetPropsType> = ({ widgetInfo }) => {
                     id={ widgetInfo.id }
                     city={ widgetInfo.city }
                     country={ widgetInfo.country }
-                    date={ currentWeather.dt_txt }
+                    date={ formatedDate }
                     temp={ currentWeather.main.temp }
                     icon={ currentWeather.weather[0].icon }
                 />
                 <Chart 
-                        
                 />
                 <BottomWidget
                     id={ widgetInfo.id }
